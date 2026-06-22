64f784f753494cfa3b61d30107d7778577ba5a4d
Shivam Kumar Prince <shivprince1718@gmail.com>
2026-06-22 06:59:54 +0000

    day 1 ja

---
+++ Javascript/01_day1.js
@@ -0,0 +1,43 @@
+console.log("First lecture of js");
+
+//diff between let, var, const
+
+// let a=87             //can be reassigned nd re-declared but scope is block limited
+// a=90
+// let a=56
+// // console.log(a);
+
+// var b=34           // can be reassigned nd re-declared and it has global scope
+// b=54
+// var b=94
+// // console.log(b);
+
+// const c=89          //can't be reassigned nd re-declared and it's scope is also upto block level
+// // c=34
+// // console.log(c);
+
+
+  
+//arithmatic operators
+let x=7
+let y=8
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+
+
+//comparison operators
+
+let x1=7
+let y1=8
+console.log(x1==y1);
+console.log(x1===y1);
+console.log(x1!==y1);
+console.log(x1<=y1);
+console.log(x1>=y1);
+console.log(x1>y1);
+console.log(x1<y1);
+
+
